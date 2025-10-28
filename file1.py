0f02fa9a5d57a80e574d2ee92e84767f9c4bb93f
Yi888a <Ziyil8@nycstudents.net>
2025-10-28 09:10:31 -0400

    buddy

--- file1.py
+++ file1.py
@@ -1,5 +1,5 @@
 import json
 data = json.load(open("./movies.json", encoding="utf8"))
 movies = open("./movies.json", encoding="utf8")
-titleget = data.get("title", "Not specified")
+titleget = data.get("title", "Unknown Title")
 print(f"title: {titleget}")
