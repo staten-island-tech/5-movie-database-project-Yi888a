aee36aaa3cdddb3757fe7f051b7a4dda10e09e89
Yi888a <Ziyil8@nycstudents.net>
2025-10-30 09:26:06 -0400

    trying to figure out how to grab stuff from json

--- file1.py
+++ file1.py
@@ -1,5 +1,16 @@
+#import json
+#a=0
+#data = json.load(open("./movies.json", encoding="utf8"))
+#movies = open("./movies.json", encoding="utf8")
+##for i in range(1000000):
+   # print (data[a]['title'])
+   # a +=1
+
 import json
+a=0
 data = json.load(open("./movies.json", encoding="utf8"))
 movies = open("./movies.json", encoding="utf8")
-titleget = data.get("title", "Unknown Title")
-print(f"title: {titleget}")
+year = input("After what year would you want your movies to be displayed? ")
+for i in data(a):
+ print (data[a]['year'])
+ 
