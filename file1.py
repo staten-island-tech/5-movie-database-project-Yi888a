c65177a20256c399da38ebbbfdd97d9a4d85517e
Yi888a <Ziyil8@nycstudents.net>
2025-11-03 09:26:23 -0500

    im so smart

--- file1.py
+++ file1.py
@@ -6,11 +6,22 @@
    # print (data[a]['title'])
    # a +=1
 
+#import json
+#data = json.load(open("./movies.json", encoding="utf8"))
+#movies = open("./movies.json", encoding="utf8")
+#year = input("After what year would you want your movies to be displayed? ")
+#for i in data:
+# if int(i['year']) > int(year):
+      # print (i['year'], i['title'])
+ 
 import json
-a=0
 data = json.load(open("./movies.json", encoding="utf8"))
 movies = open("./movies.json", encoding="utf8")
-year = input("After what year would you want your movies to be displayed? ")
-for i in data(a):
- print (data[a]['year'])
- 
+afteryear = input("After what year would you want your movies to be displayed? ")
+beforeyears = input ("Before what year would you want your movies to be displayed? ")
+for i in data:
+    if int(i['year']) > int(afteryear):
+      print (i['year'], i['title'])
+      for i in data:
+       if int(i['year']) < int(beforeyears):
+         print (i['year'], i['title'])
